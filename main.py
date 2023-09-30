deb8d4b5dacc0f857fcc2926ff911d4d5b92688b
Levaaa123t <143887693+Levaaa123t@users.noreply.github.com>
2023-09-30 18:06:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,76 @@
+from multiprocessing.spawn import get_command_line
 import discord
+from discord.ext import commands
 import random
-from bot_logic import gen_pass
 
 smiles1 = [':wave:', ':grinning:',':handshake:']
 smiles2 = [':wave:', ':saluting_face:' ,':call_me:']
-# Переменная intents - хранит привилегии бота
+parting = ['Пока, надеюсь скоро увидимся ;)', 'Пока!', 'До новых встреч!', 'Увидимся']
+greetings = ['Привет! Я тебя ждал)', 'Приветик', 'Привет, как дела?', 'Наконец то ты вернулся!:)', 'Привет! Я бот my server #5606!']
+
 intents = discord.Intents.default()
-# Включаем привелегию на чтение сообщений
 intents.message_content = True
-# Создаем бота в переменной client и передаем все привелегии
-client = discord.Client(intents=intents)
+intents.members = True
 
-@client.event
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+all_comands = ['$hello - приветствие', '$bye - прощание', '$heh (число) - отправляет смех хех', 'gen_pass (число) - генерирует пароль который ты хочешь', '$add(число + число) - скалдывает написанные числа', 
+           '$multiplication(число * число) - умножает написанные числа', '$subtraction(число - число) - вычитает написанные числа', '$division(число ÷ число) - делит написанные числа']
+
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
 
-@client.event
-async def on_message(message):
-    print(message.content)
-    if message.author == client.user:
-        return
-    if message.content.startswith("$gen_pass"):
-        character_num = int(message.content.split() [1])
-        print(character_num)
-        generated_password = gen_pass(character_num)
-        await message.channel.send(generated_password)
-        return
-    if message.content.startswith('$hello'):
-        await message.channel.send("Hi!")
-        await message.channel.send (random.choice(smiles1))
-    elif message.content.startswith('$bye'):
-        await message.channel.send("Bye bye")
-        await message.channel.send(random.choice(smiles2))
-    else:
-        await message.channel.send(message.content)
+@bot.command()
+async def hello(ctx):
+    await ctx.send(random.choice(greetings))
+    await ctx.send(random.choice(smiles1))
+
+@bot.command()
+async def bye(ctx):
+    await ctx.send(random.choice(parting))
+    await ctx.send(random.choice(smiles2))
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+@bot.command()
+async def gen_pass(ctx,pass_length):
+    elements = "+-/*!&$#?=@<>"
+    password = ""
+    pass_length = int(pass_length)
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    await ctx.send(password)
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+@bot.command()
+async def  multiplication(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left * right)
+
+@bot.command()
+async def  subtraction(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left - right)
+
+@bot.command()
+async def  division(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left / right)
+
+@bot.command()
+async def help(ctx):
+    await ctx.send(all_comands)
 
 
-client.run('')
+
+
+
+
+bot.run("ТОКЕН")
